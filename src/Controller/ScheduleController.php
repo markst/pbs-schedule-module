e7aaf2975e91e193ad07e210546bd4f16311ad79
Mark Turner <git@markst.co.uk>
2022-04-01 11:13:33 +0100

    Minor cleanup. Falls back on original schedule if `two_week` fails to resolve.

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -65,73 +65,70 @@ class ScheduleController extends ControllerBase
             }, $schedule)
         );
 
-        if ($two_week !== null) {
+        if ($two_week === null) {
             // if (count($programs) > 0) {
-
-            // Loop through the entire fortnight schedule:
-            $two_week = array_map(function ($og_program) use (
-                $programs,
-                $insomnia_lookup
-            ) {
-                // global $insomnia_lookup, $programs;
-
-                // Week 0 or 1:
-                $week = ((int) $og_program['day']) > 7;
-
-                // For each insomnia program:
-                switch ($og_program['slug']) {
-                    case 'insomnia_monday':
-                    case 'insomnia_tuesday':
-                    case 'insomnia_wednesday':
-                    case 'insomnia_thursday':
-                    case 'insomnia_friday':
-                    case 'insomnia_sunday':
-                        // do lookup on insomnialookup for correct slug:
-                        $old_slug = $og_program['slug'];
-                        $new_slug_info = $insomnia_lookup[$old_slug][$week];
-
-                        $i = array_search(
-                            $new_slug_info['slug'],
-                            array_column($programs, 'slug')
-                        );
-
-                        $new_program = $programs[$i];
-
-                        if ($new_program == null) {
-                            return $og_program;
-                        }
-
-                        // Carry existing attributes:
-                        $new_program['day'] = $og_program['day'];
-                        $new_program['start'] = $og_program['start'];
-                        $new_program['duration'] =
-                            $og_program['duration'] == null
-                                ? $og_program['duration']
-                                : 7200;
-                        $new_program['profileImage'] =
-                            $new_slug_info['profileImage'];
-
-                        // Remove stale `onairnow`:
-                        unset($new_program['onairnow']);
-                        return $new_program;
-                        break;
-                    default:
-                        // Remove unused attributes:
-                        unset($og_program['onairnow']);
-                        unset($og_program['bannerImageSmall']);
-                        unset($og_program['profileImageSmall']);
-                        unset($og_program['url']);
-                        return $og_program;
-                        break;
-                }
-            },
-            $two_week);
-
-            return $two_week;
-        } else {
             // Fallback on original $schedule?
-            return null;
+            return $schedule;
         }
+
+        // Loop through the entire fortnight schedule:
+        return array_map(function ($og_program) use (
+            $programs,
+            $insomnia_lookup
+        ) {
+            // global $insomnia_lookup, $programs;
+
+            // Week 0 or 1:
+            $week = ((int) $og_program['day']) > 7;
+
+            // For each insomnia program:
+            switch ($og_program['slug']) {
+                case 'insomnia_monday':
+                case 'insomnia_tuesday':
+                case 'insomnia_wednesday':
+                case 'insomnia_thursday':
+                case 'insomnia_friday':
+                case 'insomnia_sunday':
+                    // do lookup on insomnialookup for correct slug:
+                    $old_slug = $og_program['slug'];
+                    $new_slug_info = $insomnia_lookup[$old_slug][$week];
+
+                    $i = array_search(
+                        $new_slug_info['slug'],
+                        array_column($programs, 'slug')
+                    );
+
+                    $new_program = $programs[$i];
+
+                    if ($new_program == null) {
+                        return $og_program;
+                    }
+
+                    // Carry existing attributes:
+                    $new_program['day'] = $og_program['day'];
+                    $new_program['start'] = $og_program['start'];
+                    $new_program['duration'] =
+                        $og_program['duration'] == null
+                            ? $og_program['duration']
+                            : 7200;
+                    $new_program['profileImage'] =
+                        $new_slug_info['profileImage'];
+
+                    // Remove stale `onairnow`:
+                    unset($new_program['onairnow']);
+                    return $new_program;
+                    break;
+                default:
+                    // Remove unused attributes:
+                    unset($og_program['onairnow']);
+                    unset($og_program['bannerImageSmall']);
+                    unset($og_program['profileImageSmall']);
+                    unset($og_program['url']);
+                    return $og_program;
+                    break;
+            }
+        },
+        $two_week);
     }
 
     /**

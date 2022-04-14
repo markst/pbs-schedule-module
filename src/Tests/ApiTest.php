1368cb3db2b36bae28a5c21d26946fa4af34809e
Mark Turner <git@markst.co.uk>
2022-04-14 21:24:54 +0100

    Beginning `ApiTest`.

---
+++ src/Tests/ApiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\api_proxy_pbs\Tests;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests for the PBS Api module.
+ *
+ * @group api_proxy_pbs
+ */
+class ApiTests extends BrowserTestBase
+{
+    /**
+     * Modules to install
+     *
+     * @var array
+     */
+    public static $modules = ['api_proxy_pbs'];
+
+    // Perform initial setup tasks that run before every test method.
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Tests that the fortnight endpoint can be reached.
+     */
+    public function testFortnightEndpointExists()
+    {
+        $this->drupalGet('api/fortnight');
+        $this->assertResponse(200);
+    }
+}
